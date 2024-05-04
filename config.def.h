7b667e9c1fd815f3b1d59016957cc09369a068f9
Azmain Biswas <azmainbiswas007@gmail.com>
2024-05-05 02:29:14 +0530

    color change

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#ffffff", "#000000" },
-	[SchemeSel] = { "#000000", "#ABCDEF" },
+	[SchemeSel] = { "#ffffff", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
