671587c7d32f16caf8e10a6418b06d68cd8ad5c2
Azmain Biswas <azmainbiswas007@gmail.com>
2024-05-06 02:11:37 +0530

    change font size

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't us
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"JetBrainsMono Nerd Font:size=13",
-    "Noto Color Emoji:pixelsize=20:antialias=true:autohint=true"
+    "Noto Color Emoji:pixelsize=25:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
