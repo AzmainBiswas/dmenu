d880bf54bb4cc7bc04ad307aadaffe9f1b4545c7
Azmain Biswas <azmainbiswas007@gmail.com>
2024-05-15 19:41:37 +0530

    chenge color

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#ffffff", "#000000" },
-	[SchemeSel] = { "#ffffff", "#005577" },
+	[SchemeSel] = { "#ffffff", "#458588" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
